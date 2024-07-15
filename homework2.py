cbf2cfd7ab20dd003dfd597dece3631a9c5538a5
Alenka610 <levchukalena2019@yandex.ru>
2024-07-15 22:51:59 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+number_of_homework = '12'
+print(number_of_homework)
+number_of_hours = '1.5'
+print(number_of_hours)
+course_name = 'phyton'
+print(course_name)
+time_for_one_task = '1.5 / 12'
+print(1.5/12)
+print(course_name, ', всего задач:', number_of_homework,', затрачено часов:',number_of_hours, ', среднее время выполнения:', time_for_one_task , 'часа')
