0e59ed78debfff571b7274829c276bdd4136a6da
Alenka610 <levchukalena2019@yandex.ru>
2024-07-15 16:31:40 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print (9**0.5*5) # "1st program"
-print (9.99 > 9.98 and 1000 != 1000.1) # "2st program"
-print (1234 % 1000 // 10, 5678 % 1000 // 10, 23+67)# "3st program"
-print (13.42 , 42.13 ,) # 4th program
-print (((13.42 * 100) // 100) == ((42.13 * 100) % 100))
+a = 'Топинамбур'
+print (a [0])
+print (a [-1])
+print (a [5:])
+print (a [ : : -1])
+print (a [1:10:2])
