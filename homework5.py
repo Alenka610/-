02c71d70b023540daa6a58e0edc998933c80446c
Alenka610 <levchukalena2019@yandex.ru>
2024-07-18 17:28:26 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = 1,2,3, 'banana'
+print(immutable_var)
+immutable_var = 'кортежи, неизменяемые списки'
+print(immutable_var)
+mutable_list = [1, 2, 3, "апельсин"]
+print(mutable_list)
+mutable_list[3] = 'яблоко'
+print(mutable_list)
