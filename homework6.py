9acc476263b57f3ca2565ae7829757a57dd36723
Alenka610 <levchukalena2019@yandex.ru>
2024-07-20 22:05:21 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Alyona': 1993, 'Anton': 1992, 'Pavel': 2017, 'Alexandra': 2022}
+print(my_dict)
+print(my_dict ['Alyona'])
+print(my_dict.get('Anastasiya'))
+my_dict.update({'Natalya': 1971, 'Olga': 1969})
+print(my_dict)
+my_dict.pop('Olga')
+print(my_dict)
+my_set = {1,2,3,4,5,6,1,2,3,3,4, 'Alyona', 'Anton'}
+print(my_set)
+my_set.update({'Pavel', 'Alexandra', 'Natalya'})
+print(my_set)
+my_set.remove('Natalya')
+print(my_set)
